ea6bacb302e2bfff93b869ec09ddc4f3936f7629
David Tan <example.com>
2018-10-28 09:23:52 -0500

    add

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "travisterraformtest-195499643157-eu-central-1"
+    bucket         = "travisterraformtest-us-east-1"
     key            = "terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "travisterraformtest-195499643157-eu-central-1"
+    region         = "us-east-1"
+    dynamodb_table = "travisterraformtest-us-east-1"
   }
 }
